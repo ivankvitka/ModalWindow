3b2763c9be4f153bcec867a3beb99abe64b7048c
ivankvitka <ivan.kvitka@gmail.com>
2018-11-20 06:56:09 +0200

    added project

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync'),
+	plumber = require("gulp-plumber");
+
+gulp.task("sass", function() {
+	gulp
+		.src("app/scss/**/*.scss")
+		.pipe(plumber())
+		.pipe(sass({ outputStyle: "expanded" }))
+		.pipe(gulp.dest("app/css"))
+		.pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function () {
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('build', function() {
+
+	var buildCss = gulp.src([
+		'app/css/*.css',
+	])
+		.pipe(gulp.dest('dest/css'))
+
+	var buildHtml = gulp.src('app/*.html')
+		.pipe(gulp.dest('dest'));
+});
